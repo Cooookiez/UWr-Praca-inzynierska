85b37e10a338492a74f1a9b4acbb0db60d7f4652
Krzysiek (Chris) <kukizk@gmail.com>
2022-02-20 20:58:04 +0100

    LaTex

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
-
-valid_image_exts = [".jpg", ".jpeg", ".png"]
-
 # Get a reference to webcam #
 # (the default one)
 # for mac: 1
 # for pi: 0
 CAM_REF = 1
 
-
+valid_image_exts = [".jpg", ".jpeg", ".png"]
 KNOW_PEOPLE_DIR_PATH_NAME = "known_people"
 JSON_FILE_NAME = "faces.json"
 UNKNOWN_NAME = "Unknown"
