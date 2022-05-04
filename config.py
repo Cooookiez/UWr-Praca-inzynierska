8bbda4b5be447555e3a921d1af20863a37be796d
Krzysiek (Chris) <kukizk@gmail.com>
2022-05-04 17:57:12 +0200

    camera rotation

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
+import config_helper as ch
 # Get a reference to webcam #
 # (the default one)
 # for mac: 1
 # for pi: 0
 CAM_REF = 1
-CAM_ROTATION = 0 # degree
+CAM_ROTATION = ch.rotate[0] # 0, 90, 180, 270
 
 valid_image_exts = [".jpg", ".jpeg", ".png"]
 KNOW_PEOPLE_DIR_PATH_NAME = "known_people"
